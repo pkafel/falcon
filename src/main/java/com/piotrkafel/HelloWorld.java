7e98e7a9d9e677beddfb152555fcca21223047d2
pkafel <piotr.kafel@gmail.com>
2015-03-14 23:21:27 +0100

    Comparing two jsons

--- src/main/java/com/piotrkafel/HelloWorld.java
+++ src/main/java/com/piotrkafel/HelloWorld.java
@@ -2,14 +2,32 @@ package com.piotrkafel;
 
 import static spark.Spark.*;
 
-import spark.*;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.skyscreamer.jsonassert.JSONAssert;
 
 public class HelloWorld {
 
     public static void main(String[] args) {
 
-        get("/hello", (request, response) -> {
-            return "Hello World!";
+        get("/hello", (request, response) -> "Hello World!");
+
+        post("/compare", (request, respone) -> {
+            final String body = request.body();
+            final JSONObject jsonObject = new JSONObject(body);
+            final JSONObject firstJson = jsonObject.getJSONObject("firstJson");
+            final JSONObject secondJson = jsonObject.getJSONObject("secondJson");
+
+            return areJsonsEqual(firstJson, secondJson);
         });
+    }
+
+    private static boolean areJsonsEqual(JSONObject firstJson, JSONObject secondJson) {
+        try {
+            JSONAssert.assertEquals(firstJson, secondJson, true);
+            return true;
+        } catch (JSONException | AssertionError e) {
+            return false;
+        }
     }
 }
