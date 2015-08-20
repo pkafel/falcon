43af3a71e0c1c8cab6c654a67480149d65f12534
pkafel <piotr.kafel@gmail.com>
2015-08-20 23:01:14 +0200

    #12 Hotkeys

--- js/index.js
+++ js/index.js
@@ -17,27 +17,56 @@ function showValidationError(ex) {
   $('#json2').parent().toggleClass('has-error', ex.rightError != undefined);
 }
 
+function setHotkeys() {
+    $("#json1").bind('keydown', 'tab', function(e) {
+        e.preventDefault();
+        $('#json2').focus();
+    });
+    $("#json2").bind('keydown', 'tab', function(e) {
+        e.preventDefault();
+        $('#json1').focus();
+    });
+    $("#json1").bind('keydown', 'ctrl+return', function(e) {
+        submitForm(e);
+        $("#back-button").focus();
+    });
+    $("#json2").bind('keydown', 'ctrl+return', function(e) {
+        submitForm(e);
+        $("#back-button").focus();
+    });
+    $(document).bind('keydown', 'ctrl+return', function(e) {
+        submitForm(e);
+        $("#back-button").focus();
+    });
+    $("#back-button").bind('keydown', 'backspace', function(e) {
+        e.preventDefault();
+        showCompareForm();
+    });
+}
+
+function submitForm(event) {
+    /* stop form from submitting normally */
+    event.preventDefault();
+
+    var diff_rep = $('#diff-rep');
+    diff_rep.text('');
+
+    try {
+        var strategy = $('#strategy').val() === '1' ? new ComparingKeyStrategy() : new ComparingValueStrategy();
+        var diff = getDiffRepresentation($('#json1').val(), $('#json2').val(), strategy);
+
+        printJsonDiff(diff, 1, diff_rep);
+        showDiffForm();
+    } catch (ex) {
+        showValidationError(ex);
+    }
+}
+
 $(document).ready(function () {
     showCompareForm();
+    setHotkeys();
 
-    $("#compare-form").submit(function (event) {
-        /* stop form from submitting normally */
-        event.preventDefault();
-
-        var diff_rep = $('#diff-rep');
-        diff_rep.text('');
-
-        try {
-          var strategy = $('#strategy').val() === '1' ? new ComparingKeyStrategy() : new ComparingValueStrategy();
-          var diff = getDiffRepresentation($('#json1').val(), $('#json2').val(), strategy);
-
-          printJsonDiff(diff, 1, diff_rep);
-          showDiffForm();
-        } catch (ex) {
-          showValidationError(ex);
-        }
-    });
-
+    $("#compare-form").submit(submitForm);
     $("#back-button").click(function () {
         showCompareForm();
     });
