fca5b55a2268df016a186f9e89a938f66a42048a
pkafel <piotr.kafel@gmail.com>
2015-09-08 21:15:59 +0200

    Fix different indent for rows with and without changes

--- js/json-diff-presenter.js
+++ js/json-diff-presenter.js
@@ -4,8 +4,7 @@ INDENT_INCREASE = 4;
 OPERATION_2_CLASS_NAME = {
     '+': 'added-line-div',
     '-': 'removed-line-div',
-    '': 'unchanged-line-div',
-    '&nbsp': 'unchanged-line-div'
+    '*': 'unchanged-line-div'
 };
 
 //// UTILS
@@ -53,7 +52,7 @@ function printJsonDiff(obj, ind, div) {
     function _getOperationSignFromName(operationName) {
         if(operationName === 'ADD') return '+';
         else if(operationName === 'REMOVE') return '-';
-        else return '&nbsp;';
+        else return '*';
     }
 
     function _traverseAndPrintJsonDiff(json, ind, div) {
@@ -62,38 +61,41 @@ function printJsonDiff(obj, ind, div) {
                 return ((i + 1 < json.length) ? ',' : '');
             }
             var singleDiff = json[i];
-            var operation = _getOperationSignFromName(singleDiff.op);
 
             if(singleDiff.op === 'NONE') {
                 if(singleDiff.valueType === 'SCALAR') {
-                    _appendToTheOutput(div, operation, indent(ind) + getJsonKey(singleDiff.key) + getJsonValue(singleDiff.value) + comma());
+                    _appendToTheOutput(div, singleDiff.op, indent(ind) + getJsonKey(singleDiff.key) + getJsonValue(singleDiff.value) + comma());
                 } else if(singleDiff.valueType === 'ARRAY') {
-                    _appendToTheOutput(div, operation, indent(ind) + getJsonKey(singleDiff.key) + '[');
+                    _appendToTheOutput(div, singleDiff.op, indent(ind) + getJsonKey(singleDiff.key) + '[');
                     _traverseAndPrintJsonDiff(singleDiff.value, ind + INDENT_INCREASE, div);
-                    _appendToTheOutput(div, operation, indent(ind) + ']' + comma());
+                    _appendToTheOutput(div, singleDiff.op, indent(ind) + ']' + comma());
                 } else {
-                    _appendToTheOutput(div, operation, indent(ind) + getJsonKey(singleDiff.key) + '{');
+                    _appendToTheOutput(div, singleDiff.op, indent(ind) + getJsonKey(singleDiff.key) + '{');
                     _traverseAndPrintJsonDiff(singleDiff.value, ind + INDENT_INCREASE, div);
-                    _appendToTheOutput(div, operation, indent(ind) + '}' + comma());
+                    _appendToTheOutput(div, singleDiff.op, indent(ind) + '}' + comma());
                 }
             } else {
                 if(singleDiff.valueType === 'ARRAY') {
-                    _appendToTheOutput(div, operation, indent(ind) + getJsonKey(singleDiff.key) + '[');
-                    _printRestOfTheJsonWithOperation(singleDiff.value, operation, ind + INDENT_INCREASE, div);
-                    _appendToTheOutput(div, operation,  indent(ind) + ']' + comma());
+                    _appendToTheOutput(div, singleDiff.op, indent(ind) + getJsonKey(singleDiff.key) + '[');
+                    _printRestOfTheJsonWithOperation(singleDiff.value, singleDiff.op, ind + INDENT_INCREASE, div);
+                    _appendToTheOutput(div, singleDiff.op,  indent(ind) + ']' + comma());
                 } else if(singleDiff.valueType === 'OBJECT') {
-                    _appendToTheOutput(div, operation, indent(ind) + getJsonKey(singleDiff.key) + '{');
-                    _printRestOfTheJsonWithOperation(singleDiff.value, operation, ind + INDENT_INCREASE, div);
-                    _appendToTheOutput(div, operation, indent(ind) + '}' + comma());
+                    _appendToTheOutput(div, singleDiff.op, indent(ind) + getJsonKey(singleDiff.key) + '{');
+                    _printRestOfTheJsonWithOperation(singleDiff.value, singleDiff.op, ind + INDENT_INCREASE, div);
+                    _appendToTheOutput(div, singleDiff.op, indent(ind) + '}' + comma());
                 } else {
-                    _appendToTheOutput(div, operation, indent(ind) + getJsonKey(singleDiff.key) + getJsonValue(singleDiff.value) + comma());
+                    _appendToTheOutput(div, singleDiff.op, indent(ind) + getJsonKey(singleDiff.key) + getJsonValue(singleDiff.value) + comma());
                 }
             }
         }
     }
 
     function _appendToTheOutput(div, operation, text) {
-        div.append('<div class="' + OPERATION_2_CLASS_NAME[operation] +'">' + operation + text + '<br /></div>');
+        var operationSign = _getOperationSignFromName(operation);
+        var operationSpanVisibility = operation === 'NONE' ? 'hidden' : 'visible';
+        var operationSpan = '<span style="visibility: ' + operationSpanVisibility + '">' + operationSign + '</span>';
+
+        div.append('<div class="' + OPERATION_2_CLASS_NAME[operationSign] +'">' + operationSpan + text + '<br /></div>');
     }
 
     if (obj.topType === 'ARRAY') {
